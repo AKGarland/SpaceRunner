c740cb96d9d2efb31deca1f6ac9914bc7712079b
Garlandos <amberkgarland@gmail.com>
2017-06-23 10:34:44 +0100

    Add files via upload

---
+++ Eyes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+    private Camera eyes;
+    private float defaultFOV;
+
+	// Use this for initialization
+	void Start () {
+        eyes = GetComponent<Camera>( );
+        defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Zoom"))
+        {
+            Zoom( );
+        }
+        else if (Input.GetButtonUp("Zoom"))
+        {
+            eyes.fieldOfView = defaultFOV;
+        }
+	}
+
+    void Zoom( )
+    {
+        eyes.fieldOfView = (defaultFOV/1.5f);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject playerSpawn;
+    public bool reSpawn = false;
+
+    private Transform[] spawns;
+    private Vector3 randomSpawn;
+
+	// Use this for initialization
+	void Start () {
+        spawns = playerSpawn.GetComponentsInChildren<Transform>( );
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (reSpawn == true)
+        {
+            Respawn( );
+        }
+    }
+
+    void Respawn( )
+    {
+        randomSpawn = spawns[Random.Range(1, (spawns.Length))].transform.position;
+        transform.position = randomSpawn;
+        reSpawn = false;
+    }
+}
